41316eb780f09c54446bc69f057eacb1cf894b93
sangeeta <sangeetakhtr@gmail.com>
2025-02-18 11:03:02 +0530

    complete

---
+++ arithmetic.js
@@ -0,0 +1,46 @@
+// let a=12;
+// let b=2;
+//console.log("sum of nos:",a+b,"differece is",a-b);
+//let str1=("sum of nos");
+//let str2=("differece of nos");
+//console.log(str1,a+b);
+//console.log(str2,a-b);
+//let c=a*b;
+//let d=a/b;
+//console.log(c);
+//console.log("division is:",d);
+//console.log(2**3);
+ //a+=1;
+//console.log(a);
+//b-=1;
+
+//console.log(b);
+//a*=2;
+//console.log(a);
+//a/=2;
+//console.log(a);
+
+
+// console.log(a>b);
+// console.log(a<b);
+// let x="5";
+// let y=5;
+// console.log(x==y);
+// console.log(true||false)
+// console.log(true&&false)
+
+//  let a=40;
+//  (a%2==0)?console.log("a is even"):("a is odd");
+//  let age=16;
+//  (age>0&&age<=12)?console.log("child"):(age>13&&age<=19)?console.log("teenager"):
+//  (age>20&&age<=50)?console.log("adult"):console.log("old age")
+// let a=prompt("enter a no");
+// (a%2==0&&a%5==0)?console.log("divisible by 2&5"):(a%3==0&&a%5==0)?console.log("divisible by 3&5"):
+//  (a%2==0&&a%7==0)?console.log("divisible by 2&7"):console.log("invalid")
+let a=5;
+// b=++a;
+// console.log(b);
+
+console.log(a++);
+c=a++;
+console.log(c);
